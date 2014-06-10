bb2afdf58824e29ef8697b88eb13cc4cc9f3ea15
Dominic Barnes <dominic@dbarnes.info>
2014-06-10 12:36:57 -0400

    found a much more reliable fallback

--- index.js
+++ index.js
@@ -14,18 +14,13 @@ module.exports = function (form, name) {
         throw new TypeError("An HTML form element is required");
     }
 
-    var input = form.elements.namedItem(name);
+    var input = "namedItem" in form
+        ? form.namedItem(name)
+        : form.elements.namedItem(name);
 
     if (!input) {
         throw new RangeError("Form does not contain element with name: " + name);
     }
 
-    // HACK: IE9 only retrieves a single element when using namedItem (even if it's
-    // a checkbox/radio button which may have multiple elements with the same name)
-    if (input.type === "checkbox" || input.type === "radio") {
-        var retry = form.elements.item(name);
-        if (retry) return retry;
-    } else {
-        return input;
-    }
+    return input;
 };
